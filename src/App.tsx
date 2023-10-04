4427a0bbdfcdc0e1f90fbcfd7a300a5f998c37dd
Riley Flynn <riley@rileyflynn.me>
2023-10-03 21:52:33 -0230

    No more white flash

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
     return (
-        <div className="flex h-full w-full items-center justify-center bg-zinc-900 font-mono text-2xl font-black text-emerald-500">
+        <div className="flex h-full w-full items-center justify-center  font-mono text-2xl font-black text-emerald-500">
             Placeholder
         </div>
     );
