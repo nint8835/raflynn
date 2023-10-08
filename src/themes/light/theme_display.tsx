732f3665792a42778c07f22164842741b15ebd35
Riley Flynn <riley@rileyflynn.me>
2023-10-08 15:09:07 -0230

    Consistency

--- src/themes/light/theme_display.tsx
+++ src/themes/light/theme_display.tsx
@@ -2,6 +2,6 @@ import { BaseThemeDisplay } from '@/themes/common/theme_display';
 
 export const ThemeDisplay = BaseThemeDisplay({
     containerClasses: 'bg-zinc-300',
-    titleClasses: 'text-black',
-    subtitleClasses: 'text-black',
+    titleClasses: 'text-zinc-800',
+    subtitleClasses: 'text-zinc-700',
 });
