2662b4b3becdd29ef7c8105c885358a8a7d6a00c
Riley Flynn <riley@rileyflynn.me>
2023-10-10 23:15:26 -0230

    Wrong div

--- src/themes/common/root_layout.tsx
+++ src/themes/common/root_layout.tsx
@@ -7,11 +7,18 @@ import type { RootLayoutProps } from '@/themes/definition';
 export function BaseRootLayout(options: { mainClasses: string }): React.FC<RootLayoutProps> {
     return function RootLayout({ children }: RootLayoutProps) {
         return (
-            <div className={clsx('grid h-fit min-h-screen grid-cols-1 gap-2 p-2 lg:grid-cols-2', options.mainClasses)}>
-                {children}
+            <>
+                <div
+                    className={clsx(
+                        'grid h-fit min-h-screen grid-cols-1 gap-2 p-2 lg:grid-cols-2',
+                        options.mainClasses,
+                    )}
+                >
+                    {children}
+                </div>
                 <ThemeDisplay />
                 {import.meta.env.MODE === 'development' && <DevThemeSelector />}
-            </div>
+            </>
         );
     };
 }
